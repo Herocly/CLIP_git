5f0a8076323b63d0c66520c274660f229717eda4
herocly <zzyherocly@outlook.com>
2025-04-14 15:30:57 +0800

    提交以下

--- model/detect.py
+++ model/detect.py
@@ -122,7 +122,7 @@ def class_demo_strawberry_post(path , text_language):
                     "叶斑病草莓",
                     "白粉病草莓",
                     "正常的草莓果实",
-                    "正常的草莓植株1",
+                    "正常的草莓植株",
                     "该张照片和草莓无关",
     ]
     # 测试分类的dem
