bf2dbeaca5d1c4f5a5a153022a4f1641d383fc3b
Liyilyri <2129494706@qq.com>
2025-04-15 16:39:18 +0800

    move print to __main__

--- model/getLabelFromBLM.py
+++ model/getLabelFromBLM.py
@@ -8,39 +8,6 @@ client = OpenAI(
   api_key="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",
 )
 
-completion = client.chat.completions.create(
-  extra_headers={
-    "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
-    "X-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
-  },
-  extra_body={},
-  model="openai/gpt-4.1",
-  messages=[
-    {
-      "role": "user",
-      "content": [
-        {
-          "type": "text",
-          "text": "你是什么模型"
-        }
-        # {
-        #   "type": "image_url",
-        #   "image_url": {
-        #     "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
-        #   }
-        # }
-      ]
-    }
-  ]
-)
-
-
-def ask_gpt4(prompt: str):
-    response = client.chat.completions.create(
-    model="openai/gpt-4.1",
-    messages=[{"role": "user", "content": prompt}]
-    )
-    return response.choices[0].message.content
 
 def gpt_labs(prompt: str):
 
@@ -65,9 +32,9 @@ def gpt_labs(prompt: str):
             I am training a model using contrastive image-text learning.
             Please act as an image description generator.
 
-            Given the class name '{prompt}', generate 10 unique short descriptions.
+            Given the class name '{prompt}', generate as more unique short descriptions as you can, but don't generate more than 30.
             Each description should follow this format:
-            a photo of '{prompt}', attribute
+            a photo of '{prompt}' attribute
 
             The [attribute] should describe visual traits like color, shape, condition, or scene (e.g. 'ripe and red', 'covered with white mold', 'on a wooden table').
 
